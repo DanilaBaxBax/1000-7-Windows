076b3e68ed06542bc5c5bffca42818dd64f983a9
DanilaBaxBax <daniil.kuznetsov2001@mail.ru>
2023-07-03 03:21:55 +0300

    Project upload
    Project upload

---
+++ 1000-7/1000-7.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <random>
+
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+	int random = rand();
+
+
+
+
+	setlocale(LC_ALL, "Russian");
+	for (int i = 1000; i > 6; i = i - 7)
+	{
+		cout << i << " - 7 = " <<(i - 7) << '\n';
+		Sleep(100);
+	};
+
+
+	cout << "Я гуль" << '\n';
+
+	if (random % 2 == 0)
+	{
+		cout << "Гуль советует вам проебать еще одну катку" << '\n';
+	}
+
+	else
+
+	{
+		cout << "Гуль советует вам пойти отдохнуть" << '\n';
+	}
+
+	cout << "Made by DanilaBaxBax" << '\n';
+	system("pause");
+
+}
